9253eb4e414840694954d1339fcf048859619a3c
Sergey <shchegolikhin.sergey@gmail.com>
2025-06-19 15:28:26 +0300

    update Dockerfile

--- server/services/auth-service/src/services/register_service.rs
+++ server/services/auth-service/src/services/register_service.rs
@@ -10,11 +10,10 @@ pub async fn register_user_by_username(
     user:User
 ) -> Result<(), sqlx::Error> {
     println!("Операция в бд!");
-    sqlx::query!(
-        "INSERT INTO users (username, password_hash) VALUES($1, $2);",
-        user.username,
-        user.password
-    )
+    sqlx::query(
+        "INSERT INTO users (username, password_hash) VALUES($1, $2);")
+        .bind(user.username)
+        .bind(user.password)
     .execute(&state.pool) 
     .await?;
     Ok(())
@@ -24,12 +23,11 @@ pub async fn register_user_by_email(
     State(state): State<Arc<AppState>>,
     user:User
 ) -> Result<(), sqlx::Error> {
-    sqlx::query!(
-        "INSERT INTO users (username, password_hash, email) VALUES($1, $2, $3);",
-        user.username,
-        user.password,
-        user.email
-    )
+    sqlx::query(
+        "INSERT INTO users (username, password_hash, email) VALUES($1, $2, $3);")
+    .bind(user.username)
+    .bind(user.password)
+    .bind(user.email)
     .execute(&state.pool) 
     .await?;
     Ok(())
@@ -41,13 +39,12 @@ pub async fn register_user_by_role(
     role:String
 ) -> Result<(), sqlx::Error> {
     println!("Операция в бд!");
-    sqlx::query!(
-        "INSERT INTO users (username, password_hash, role, email) VALUES($1, $2, $3, $4);",
-        user.username,
-        user.password,
-        role,
-        user.email
-    )
+    sqlx::query(
+        "INSERT INTO users (username, password_hash, role, email) VALUES($1, $2, $3, $4);")
+    .bind(user.username)
+    .bind(user.password)
+    .bind(user.role)
+    .bind(user.email)
     .execute(&state.pool) 
     .await?;
     Ok(())
