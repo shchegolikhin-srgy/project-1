471612e53fd0586e198e0d778f3c3151206c7bda
Sergey <shchegolikhin.sergey@gmail.com>
2025-06-26 23:50:13 +0300

    rename auth db

--- server/services/auth-service/target/debug/deps/auth_service-2a2f21285a06d190.d
+++ server/services/auth-service/target/debug/deps/auth_service-2a2f21285a06d190.d
@@ -1,5 +1,5 @@
-/home/sergey/projects/other/project-1/server/services/auth-service/target/debug/deps/auth_service-2a2f21285a06d190: src/main.rs
+/home/sergey/projects/other/social/server/services/auth-service/target/debug/deps/auth_service-2a2f21285a06d190: src/main.rs
 
-/home/sergey/projects/other/project-1/server/services/auth-service/target/debug/deps/auth_service-2a2f21285a06d190.d: src/main.rs
+/home/sergey/projects/other/social/server/services/auth-service/target/debug/deps/auth_service-2a2f21285a06d190.d: src/main.rs
 
 src/main.rs:
