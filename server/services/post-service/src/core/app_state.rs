a04c62aa3be950f30f61d8d04c7d8317688a320f
Sergey <shchegolikhin.sergey@gmail.com>
2025-06-29 01:39:09 +0300

    dfsdf

--- server/services/post-service/src/core/app_state.rs
+++ server/services/post-service/src/core/app_state.rs
@@ -11,7 +11,20 @@ pub struct AppState {
 
 impl AppState{
     pub async fn new(settings:Settings)->Result<Self, anyhow::Error>{
-        
+        let cluster_config = NodeTcpConfigBuilder::new()
+            .with_contact_point("127.0.0.1:9042".into())
+            .build()
+            .await?;
+        let session = TcpSessionBuilder::new(RoundRobinLoadBalancingStrategy::new(), cluster_config)
+            .build()
+            .await?;
+
+        let create_ks = "CREATE KEYSPACE IF NOT EXISTS posts WITH REPLICATION = { \
+            'class' : 'SimpleStrategy', 
+            'replication_factor' : 1 };";
+        session
+            .query(create_ks)
+            .await?;
         Ok(Self {
             
          })
--- server/services/post-service/src/main.rs
+++ server/services/post-service/src/main.rs
@@ -3,10 +3,6 @@ use post_service::core::config::Settings;
 use std::sync::Arc;
 use dotenv::dotenv;
 
-use cdrs_tokio::cluster::session::{TcpSessionBuilder, SessionBuilder};
-use cdrs_tokio::cluster::NodeTcpConfigBuilder;
-use cdrs_tokio::load_balancing::RoundRobinLoadBalancingStrategy;
-use cdrs_tokio::query::*;
 
 #[tokio::main]
 async fn main()-> Result<(), Box<dyn std::error::Error>>{
@@ -14,22 +10,6 @@ async fn main()-> Result<(), Box<dyn std::error::Error>>{
     let settings = Settings::new().await?;
     let app_state = AppState::new(settings.clone()).await?; 
     let state = Arc::new(app_state);
-
-
-    let cluster_config = NodeTcpConfigBuilder::new()
-        .with_contact_point("127.0.0.1:9042".into())
-        .build()
-        .await?;
-    let session = TcpSessionBuilder::new(RoundRobinLoadBalancingStrategy::new(), cluster_config)
-        .build()
-        .await?;
-
-    let create_ks = "CREATE KEYSPACE IF NOT EXISTS posts WITH REPLICATION = { \
-                    'class' : 'SimpleStrategy', 
-                    'replication_factor' : 1 };";
-    session
-        .query(create_ks)
-        .await?;
 
     run_server(state, settings).await?;
     Ok(())
